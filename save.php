36bf6d2b2097a0d8c0ec9473bf26d0666f50ac4e
ArtemKo7v <artemk@211.ru>
2015-09-23 17:17:22 +0700

    Changes  saving script

--- save.php
+++ save.php
@@ -1,7 +1,18 @@
 <?php
 $dir = dirname(__FILE__);
-$content = $_POST['locale'];
-$user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] . '-' : '';
-copy($dir . '/locales.js', $dir . '/backup/locales-' . $user . time() . '.js.bak');
-file_put_contents($dir . '/locales.js', $content);
+
+// Save locale file
+if(isset($_POST['locale'])){
+    $content = $_POST['locale'];
+    $user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] . '-' : '';
+    copy($dir . '/locales.js', $dir . '/backup/locales-' . $user . time() . '.js.bak');
+    file_put_contents($dir . '/locales.js', $content);
+}
+
+// Save changes
+if(isset($_POST['changes'])){
+    $content = $_POST['changes'];
+    file_put_contents($dir . '/locales-changes.js', $content);
+}
+
 echo 'OK';
